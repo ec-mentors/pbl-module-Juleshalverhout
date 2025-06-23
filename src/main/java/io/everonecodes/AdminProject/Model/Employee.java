a9f23b2cf31b28b82caf869d2599d8204265fd3d
halverhoutjules <halverhout.jules@gmail.com>
2025-06-23 14:33:00 +0200

    AdminProject JPA entities, small check

---
+++ src/main/java/io/everonecodes/AdminProject/Model/Employee.java
@@ -0,0 +1,38 @@
+package io.everonecodes.AdminProject.Model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long employeeId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String employeeFunction;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "employee_project_assignments",
+            joinColumns = @JoinColumn(name = "employeeId"),
+            inverseJoinColumns = @JoinColumn(name = "projectId")
+    )
+    private Set<Project> assignedProjects = new HashSet<>();
+}
---
+++ src/main/java/io/everonecodes/AdminProject/Model/Project.java
@@ -0,0 +1,40 @@
+package io.everonecodes.AdminProject.Model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long projectId;
+
+    @Column(nullable = false)
+    private String projectName;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String location;
+
+    private boolean isAssigned;
+
+    @Column(nullable = false)
+    private LocalDateTime creationDate = LocalDateTime.now();
+
+    @ManyToMany(mappedBy = "assignedProjects", fetch = FetchType.LAZY)
+
+    private Set<Employee> assignedEmployees = new HashSet<>();
+}
